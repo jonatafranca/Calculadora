c1bb104c64e25c45d34b4659c0349f389fe47add
Jonatã França <jonatafranca.eq@gmail.com>
2021-07-12 02:17:48 -0300

    test multiplication

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,24 +4,50 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:calc_oficina/main.dart';
 
 void main() {
-  testWidgets('contagem de botoes', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('verificação de botões da calculadora',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    //Verificação dos botões
+    //Verificação geral da quantidade
+    expect(find.byType(MaterialButton), findsNWidgets(27));
+    //Verificação específica, números = 10*2
+    expect(find.widgetWithText(MaterialButton, '1'), findsNWidgets(2));
+    expect(find.widgetWithText(MaterialButton, '2'), findsNWidgets(2));
+    expect(find.widgetWithText(MaterialButton, '3'), findsNWidgets(2));
+    expect(find.widgetWithText(MaterialButton, '4'), findsNWidgets(2));
+    expect(find.widgetWithText(MaterialButton, '5'), findsNWidgets(2));
+    expect(find.widgetWithText(MaterialButton, '6'), findsNWidgets(2));
+    expect(find.widgetWithText(MaterialButton, '7'), findsNWidgets(2));
+    expect(find.widgetWithText(MaterialButton, '8'), findsNWidgets(2));
+    expect(find.widgetWithText(MaterialButton, '9'), findsNWidgets(2));
+    expect(find.widgetWithText(MaterialButton, '0'), findsNWidgets(2));
+    //Verificação específica, operadores = 5
+    expect(find.widgetWithText(MaterialButton, '+'), findsOneWidget);
+    expect(find.widgetWithText(MaterialButton, '-'), findsOneWidget);
+    expect(find.widgetWithText(MaterialButton, '*'), findsOneWidget);
+    expect(find.widgetWithText(MaterialButton, '/'), findsOneWidget);
+    expect(find.widgetWithText(MaterialButton, '%'), findsOneWidget);
+    //Verificação específica, ações = 2
+    expect(find.widgetWithText(MaterialButton, 'Calcular'), findsOneWidget);
+    expect(find.widgetWithText(MaterialButton, 'Zerar'), findsOneWidget);
+  });
+  testWidgets('verificação de resultados de multiplicação',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
-    // // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
-
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
-
-    //Verificar os botões
-    expect(find.text('Operação: '), findsOneWidget);
-    expect(find.text('Resultado: '), findsOneWidget);
+    //Busca por valores na tela
+    expect(find.widgetWithText(Text, '12'), findsNothing);
+    //Execução de operações
+    //Erro, a função tap encontra dois botões iguais
+    await tester.tap(find.widgetWithText(MaterialButton, '4'));
+    await tester.pump();
+    await tester.tap(find.widgetWithText(MaterialButton, '*'));
+    await tester.pump();
+    // await tester.tap(find.widgetWithText(MaterialButton, '3'));
+    await tester.pump();
+    await tester.tap(find.widgetWithText(MaterialButton, 'Calcular'));
+    await tester.pump();
+    //Busca por resultados
+    expect(find.widgetWithText(Text, '12'), findsOneWidget);
   });
 }
