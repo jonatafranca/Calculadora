23975f62334d30ed8207d6ee8f87618cfff7c5ab
Jonatã França <jonatafranca.eq@gmail.com>
2021-07-12 11:57:26 -0300

    test clean screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -30,11 +30,14 @@ void main() {
     //Verificação específica, ações = 2
     expect(find.widgetWithText(MaterialButton, 'Calcular'), findsOneWidget);
     expect(find.widgetWithText(MaterialButton, 'Zerar'), findsOneWidget);
+    //Verificação específica, avisos
+    expect(find.text('Resultado: '), findsOneWidget);
+    expect(find.text('Operação: '), findsOneWidget);
   });
   testWidgets('verificação de limpeza da tela', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
     //Busca por valores na tela
-    expect(find.widgetWithText(Text, '13579'), findsNothing);
+    expect(find.text('13579'), findsNothing);
     //Execução de operações
     await tester.tap(find.widgetWithText(MaterialButton, '1').first);
     await tester.pump();
@@ -47,12 +50,12 @@ void main() {
     await tester.tap(find.widgetWithText(MaterialButton, '9').first);
     await tester.pump();
     //Busca pelo valor digitado
-    expect(find.widgetWithText(Text, '13579'), findsOneWidget);
+    expect(find.text('13579  '), findsOneWidget);
     //Limpeza
     await tester.tap(find.widgetWithText(MaterialButton, 'Zerar'));
     await tester.pump();
     //Verificação de limpeza
-    expect(find.widgetWithText(Text, '13579'), findsNothing);
+    expect(find.text('13579'), findsNothing);
   });
   testWidgets('verificação de resultados de multiplicação',
       (WidgetTester tester) async {
@@ -62,7 +65,8 @@ void main() {
     await tester.tap(find.widgetWithText(MaterialButton, 'Zerar'));
     await tester.pump();
     //Busca por valores na tela
-    expect(find.widgetWithText(Text, '12'), findsNothing);
+    expect(find.text('4 * 3'), findsNothing);
+    expect(find.text('12'), findsNothing);
     //Execução de operações
     await tester.tap(find.widgetWithText(MaterialButton, '4').first);
     await tester.pump();
@@ -70,9 +74,14 @@ void main() {
     await tester.pump();
     await tester.tap(find.widgetWithText(MaterialButton, '3').last);
     await tester.pump();
+    //Busca por valores na tela
+    expect(find.text('4 * 3'), findsOneWidget);
+    expect(find.text('12'), findsNothing);
+    //Execução do comando
     await tester.tap(find.widgetWithText(MaterialButton, 'Calcular'));
     await tester.pump();
     //Busca por resultados
-    expect(find.widgetWithText(Text, '12.00'), findsOneWidget);
+    expect(find.text('4 * 3'), findsOneWidget);
+    expect(find.text('12'), findsOneWidget);
   });
 }
